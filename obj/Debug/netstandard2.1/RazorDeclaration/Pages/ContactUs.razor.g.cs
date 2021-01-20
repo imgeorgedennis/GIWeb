e25608621b1d4db1ba5c808c8df557a977536562
George Dennis <64212979+imgeorgedennis@users.noreply.github.com>
2021-01-20 17:12:56 +0000

    Fixed button turning grey on hover

--- obj/Debug/netstandard2.1/RazorDeclaration/Pages/ContactUs.razor.g.cs
+++ obj/Debug/netstandard2.1/RazorDeclaration/Pages/ContactUs.razor.g.cs
@@ -13,70 +13,70 @@ namespace GIWeb.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\George\Documents\GIWeb\_Imports.razor"
+#line 1 "C:\Users\Work\Documents\GIWeb\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\George\Documents\GIWeb\_Imports.razor"
+#line 2 "C:\Users\Work\Documents\GIWeb\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\George\Documents\GIWeb\_Imports.razor"
+#line 3 "C:\Users\Work\Documents\GIWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\George\Documents\GIWeb\_Imports.razor"
+#line 4 "C:\Users\Work\Documents\GIWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\George\Documents\GIWeb\_Imports.razor"
+#line 5 "C:\Users\Work\Documents\GIWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\George\Documents\GIWeb\_Imports.razor"
+#line 6 "C:\Users\Work\Documents\GIWeb\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\George\Documents\GIWeb\_Imports.razor"
+#line 7 "C:\Users\Work\Documents\GIWeb\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\George\Documents\GIWeb\_Imports.razor"
+#line 8 "C:\Users\Work\Documents\GIWeb\_Imports.razor"
 using GIWeb;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\George\Documents\GIWeb\_Imports.razor"
+#line 9 "C:\Users\Work\Documents\GIWeb\_Imports.razor"
 using GIWeb.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\George\Documents\GIWeb\Pages\ContactUs.razor"
+#line 2 "C:\Users\Work\Documents\GIWeb\Pages\ContactUs.razor"
 using GIWeb.Shared.Components;
 
 #line default
@@ -91,7 +91,7 @@ using GIWeb.Shared.Components;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 83 "C:\Users\George\Documents\GIWeb\Pages\ContactUs.razor"
+#line 83 "C:\Users\Work\Documents\GIWeb\Pages\ContactUs.razor"
        
 
     GIInput Address, Fullname, Phonenumber, Email;
